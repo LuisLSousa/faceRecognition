efc2721e61b37f9fc4991545ff5856c55f8c621f
LuisLSousa <luis.srl.sousa@gmail.com>
2020-12-06 21:17:27 +0000

    first commit

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return(
+        <div>
+            <p className='f4 white'>
+                {'Paste an image\'s url and let me work my magic!'}
+            </p>
+            <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text'/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-green'>Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm
